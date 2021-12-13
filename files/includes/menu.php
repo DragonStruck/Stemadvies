43fb0df3ae2bf548c0767603044b5a872b496d47
Dragon-Strike <540503@edu.rocmn.nl>
2021-12-13 15:03:32 +0100

    svg's en start van menu

--- files/includes/menu.php
+++ files/includes/menu.php
@@ -1,14 +1,21 @@
-<div class="logo">
-    <img src="/files/images/stemadvies.svg" alt="Stemadvies logo">
-    <hr>
-</div>
-
 <nav class="nav">
+    <div class="logo">
+        <img src="/files/images/stemadvies.svg" alt="Stemadvies logo">
+        <hr>
+    </div>
 
+    <div class="a-container">
+        <a href="/stellingen">
+            <button class="menu-button">Stellingen</button>
+        </a>
+        <a href="/partijen">
+            <button class="menu-button">Partijen</button>
+        </a>
+    </div>
+    <div class="logout-container">
+        <hr>
+        <a href="/logout">
+            <button class="logout-button">Uitloggen</button>
+        </a>
+    </div>
 </nav>
-<div class="logout-container">
-    <hr>
-    <a href="/logout">
-        <button class="logout-button">Uitloggen</button>
-    </a>
-</div>
