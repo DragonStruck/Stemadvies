5e113f4ec1d8711a3e431ddc856346407d9c256b
Dragon-Strike <540503@edu.rocmn.nl>
2021-12-13 13:54:13 +0100

    logout werkt

--- files/includes/menu.php
+++ files/includes/menu.php
@@ -1,9 +1,14 @@
 <div class="logo">
     <img src="/files/images/stemadvies.svg" alt="Stemadvies logo">
+    <hr>
 </div>
+
 <nav class="nav">
 
 </nav>
 <div class="logout-container">
-
+    <hr>
+    <a href="/logout">
+        <button class="logout-button">Uitloggen</button>
+    </a>
 </div>
