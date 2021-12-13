0f8d46258888db7d2810d654f73f768b195f9c83
Dragon-Strike <540503@edu.rocmn.nl>
2021-12-13 11:47:35 +0100

    login pagina html en styling

--- files/views/login.php
+++ files/views/login.php
@@ -1,15 +1,39 @@
 <?php
-?>
 
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
 
     <?php include "files/includes/head.php";?>
 
+    <link rel="stylesheet" href="/files/css/login.css">
+
     <title>Login | Stemadvies</title>
 </head>
 <body>
-
+<main class="main">
+    <div class="login-form-container">
+        <form action="/files/requests/login.php" class="login-form">
+            <div class="logo">
+                <img src="/files/images/stemadvies.svg" alt="logo">
+            </div>
+            <div class="form-inputs">
+                <div class="input-container">
+                    <label for="username">Gebruikersnaam</label>
+                    <input class="input" id="username" name="username" type="text">
+                </div>
+                <div class="input-container">
+                    <label for="password">Wachtwoord</label>
+                    <input class="input" id="password" name="password" type="password">
+                </div>
+            </div>
+            <div class="submit-container">
+                <input value="Inloggen" class="submit" id="submit-login" name="login" type="submit">
+            </div>
+        </form>
+    </div>
+</main>
 </body>
 </html>
