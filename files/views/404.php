2ae2dd8dd27f89a3719e389ddbc9c978b1e5739f
Dragon-Strike <540503@edu.rocmn.nl>
2021-12-13 10:01:48 +0100

    head

--- files/views/404.php
+++ files/views/404.php
@@ -1,1 +1,15 @@
 <?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <?php include "files/includes/head.php";?>
+
+    <title>Admin | Stemadvies</title>
+</head>
+<body>
+
+</body>
+</html>
