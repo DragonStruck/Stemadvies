74b22ded6dff9fe9493796faeed1de33c683fd50
Dragon-Strike <540503@edu.rocmn.nl>
2021-12-14 17:08:39 +0100

    Stellingen table gemaakt v1

--- files/views/partijen.php
+++ files/views/partijen.php
@@ -3,5 +3,11 @@
 ?>
 
 <div>
-    yee 2
+    <div class="logo">
+        <h1 class="title">Partijen</h1>
+        <hr>
+    </div>
+    <div>
+        yee 2
+    </div>
 </div>
--- files/views/stellingen.php
+++ files/views/stellingen.php
@@ -3,5 +3,40 @@
 ?>
 
 <div>
-    yee 1
+    <div class="logo">
+        <h1 class="title">Stellingen</h1>
+        <hr>
+    </div>
+    <div class="custom-table">
+        <header>
+            <h2>ID</h2>
+            <h2>Naam</h2>
+        </header>
+        <div class="table-content">
+            <div class="table-entry">
+                <div>1</div>
+                <div>helebst heleboel tekst heleboel tekst</div>
+                <div>
+                    <button class="table-button">Edit</button>
+                    <button class="table-button">Delete</button>
+                </div>
+            </div>
+            <div class="table-entry">
+                <div>2</div>
+                <div>heleboel tekst helebl tekst heleboel tekst</div>
+                <div>
+                    <button class="table-button">Edit</button>
+                    <button class="table-button">Delete</button>
+                </div>
+            </div>
+            <div class="table-entry">
+                <div>3</div>
+                <div>heleboel tekst helebokst</div>
+                <div>
+                    <button class="table-button">Edit</button>
+                    <button class="table-button">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
