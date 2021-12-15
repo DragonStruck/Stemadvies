f8dfff8217a91fbeabcac16d524f1c582ac5fce5
Dragon-Strike <540503@edu.rocmn.nl>
2021-12-15 15:12:37 +0100

    start van stellingen en partijen toevoegen

--- files/views/partij-add.php
+++ files/views/partij-add.php
@@ -8,6 +8,17 @@
         <hr>
     </div>
     <div>
-
+        <div>
+            <label for="naam">Naam:</label>
+            <input type="text" id="naam" name="naam">
+        </div>
+        <div>
+            <label for="afkorting">Afkorting:</label>
+            <textarea id="afkorting" name="afkorting"></textarea>
+        </div>
+        <div>
+            <button class="add-button">Partij opslaan</button>
+            <button class="add-button">Annuleren</button>
+        </div>
     </div>
 </div>
--- files/views/stelling-add.php
+++ files/views/stelling-add.php
@@ -8,6 +8,17 @@
         <hr>
     </div>
     <div>
-
+        <div>
+            <label for="onderwerp">Onderwerp:</label>
+            <input type="text" id="onderwerp" name="onderwerp">
+        </div>
+        <div>
+            <label for="stelling">Stelling:</label>
+            <textarea id="stelling" name="stelling"></textarea>
+        </div>
+        <div>
+            <button class="add-button">Stelling opslaan</button>
+            <button class="add-button">Annuleren</button>
+        </div>
     </div>
 </div>
